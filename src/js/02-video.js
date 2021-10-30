0601ac2300c6c18be81c6e940828e24cf24d1c15
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-30 21:51:20 +0300

    Добавляє рішення 2-го завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+    import VimeoPlayer from "@vimeo/player";
+    import throttle from "lodash.throttle";
 
+    const iframeRef = document.querySelector('#vimeo-player');
+    const player = new VimeoPlayer(iframeRef);
+    
+    player.on('timeupdate', throttle(function (event) {       
+        localStorage.setItem("videoplayer-current-time", event.seconds)}, 1000));
+
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
