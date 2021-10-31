164a8393d5f3addd616b903d0bfa1d4b9a545894
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-31 09:15:54 +0200

    вирішує задачу№3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const formRef = document.querySelector(".feedback-form");
+
+const inputTextForm = {};
+initForm();
+
+formRef.addEventListener("input", throttle(onTextInput, 500));
+
+formRef.addEventListener('submit', event => {
+    event.preventDefault();
+    localStorage.removeItem("feedback-form-state");
+
+    const formData = new FormData(formRef);
+    formData.forEach((value, name) => console.log(name, value));
+    
+    event.currentTarget.reset();
+});
+
+function onTextInput(event) {
+    inputTextForm[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(inputTextForm));
+}
+
+function initForm() {
+    let staticTextForm = localStorage.getItem("feedback-form-state");
+   
+    if (staticTextForm) {
+        staticTextForm = JSON.parse(staticTextForm);
+        Object.entries(staticTextForm).forEach(([name, value]) => {
+        formRef.elements[name].value = value;
+        });
+    }
+}
